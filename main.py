0360642e50a75ae4d6813d6210f24d49759a125f
Doaa Refat <doaa.refat@itworxedu.com>
2024-06-29 12:28:41 +0300

    githab actions

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+import os
+
+from flask import Flask, render_template, request, redirect, url_for, jsonify, json
 from logger import setup_logger
 
+VISITS_FILE = 'visits.json'
 app = Flask(__name__)
 log = setup_logger("app")
 
@@ -37,5 +40,31 @@ def delete_task(index):
     return redirect(url_for('index'))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+def get_visit_count():
+    if not os.path.exists(VISITS_FILE):
+        return 0
+    with open(VISITS_FILE, 'r') as f:
+        data = json.load(f)
+        return data.get('visits', 0)
+
+
+def increment_visit_count():
+
+    visitor = get_visit_count() + 1
+    with open(VISITS_FILE, 'w') as f:
+        json.dump({'visits': visitor}, f)
+
+
+
+
+@app.route('/visits')
+def visits():
+
+    visits = get_visit_count()
+    return jsonify({"visits": visits})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
+
